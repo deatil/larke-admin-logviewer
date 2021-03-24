301f2ba93f3ce2ace16ae5627d5e8ba31fec1437
deatil <2217957370@qq.com>
2021-03-24 11:59:08 +0800

    优化

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -42,7 +42,7 @@ class ServiceProvider extends BaseServiceProvider
      */
     public $icon = __DIR__ . '/../icon.png';
     
-    protected $slug = '';
+    protected $slug = 'larke-admin.extension.log-viewer';
     
     /**
      * 初始化
@@ -51,8 +51,6 @@ class ServiceProvider extends BaseServiceProvider
     {
         // 扩展注册
         Extension::extend($this->info['name'], __CLASS__);
-        
-        $this->slug = 'larke-admin.extension.log-viewer';
     }
     
     /**
